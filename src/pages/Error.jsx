bf250d44fd9c17552eacd0799c25f72656f6208f
Phonekham <phone.madridista@gmail.com>
2019-08-17 10:37:23 +0700

    Banner Component

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 const Error = () => {
   return (
     <div>
-      <Hero />
+      <Hero>
+        <Banner title="Page not found">
+          <Link to="/" className="btn-primary">
+            Return Home
+          </Link>
+        </Banner>
+      </Hero>
     </div>
   );
 };
--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 const Rooms = () => {
   return (
     <div>
-      <Hero hero="roomsHero" />
+      <Hero hero="roomsHero">
+        <Banner title="Our Rooms">
+          <Link to="/" className="btn-primary">
+            Return Home
+          </Link>
+        </Banner>
+      </Hero>
     </div>
   );
 };
