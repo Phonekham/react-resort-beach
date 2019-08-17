06bc779fbca172bfcc99959888236c5779a99547
Phonekham <phone.madridista@gmail.com>
2019-08-17 09:33:33 +0700

    setup router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
 import "./App.css";
 
 function App() {
-  return <>app</>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/" exact component={Rooms} />
+        <Route path="/single-room/:slug" exact component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
