4466b988dd65aaf790aabc670f26352355d5a915
Phonekham <phone.madridista@gmail.com>
2019-08-17 09:22:09 +0700

    Page setup

---
+++ src/pages/Rooms.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rooms = () => {
+  return (
+    <div>
+      <h2>Roomspage</h2>
+    </div>
+  );
+};
+
+export default Rooms;
---
+++ src/pages/SingleRoom.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SingleRoom extends Component {
+  render() {
+    return (
+      <div>
+        <h3>sigle room</h3>
+      </div>
+    );
+  }
+}
