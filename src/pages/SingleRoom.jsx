6984ec7ce4e53f993a6e91b9fe12e900f8e14155
Phonekham <phone.madridista@gmail.com>
2019-08-18 11:23:17 +0700

    20. Styled-Hero
    styled-components

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -4,6 +4,7 @@ import { RoomContext } from "../context";
 import defaultBcg from "../images/room-1.jpeg";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+import StyledHero from "../components/StyledHero";
 
 export default class SingleRoom extends Component {
   constructor(props) {
@@ -41,13 +42,13 @@ export default class SingleRoom extends Component {
       images
     } = room;
     return (
-      <Hero hero="roomsHero">
+      <StyledHero img={images[0]}>
         <Banner title={`${name} room`}>
           <Link to="/rooms" className="btn-primary">
             Back To Rooms
           </Link>
         </Banner>
-      </Hero>
+      </StyledHero>
     );
   }
 }
