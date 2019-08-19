e26712f463155eb4255463ed351032f0e6fa1a75
Phonekham <phone.madridista@gmail.com>
2019-08-19 11:26:26 +0700

    price filter

--- src/context.jsx
+++ src/context.jsx
@@ -88,6 +88,8 @@ export default class RoomProvider extends Component {
     if (capacity !== 1) {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
     this.setState({ sortedRooms: tempRooms });
   };
 
