b0abf9f30a448ce9e9058bd65521c21b632ae570
Phonekham <phone.madridista@gmail.com>
2019-08-19 10:01:03 +0700

    23. Room List

--- src/components/RoomsList.jsx
+++ src/components/RoomsList.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
-const RoomsList = () => {
+import Room from "./Room";
+
+const RoomsList = ({ rooms }) => {
+  if (rooms.length === 0) {
+    return (
+      <div className="empty-search">
+        <h3>Unfortunately no rooms match your search</h3>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h3>roomlist</h3>
-    </div>
+    <section className="roomslist">
+      <div className="roomslist-center" />
+      {rooms.map(item => {
+        return <Room key={item.id} room={item} />;
+      })}
+    </section>
   );
 };
 
